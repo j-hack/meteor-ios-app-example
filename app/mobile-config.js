5b2b66f298e015b7f7e4543784f6ca0d36fe2ad2
Nobutaka OSHIRO <n-oshiro@j-hack.co.jp>
2016-01-17 12:50:14 +0900

    add ios splash screen images

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -18,3 +18,17 @@ App.icons({
   'ipad'     : 'resources/icons/app_icon-76x76.png',
   'ipad_2x'  : 'resources/icons/app_icon-76x76@2x.png', // 144x144
 });
+
+App.launchScreens({
+  // iOS
+  'iphone'            : 'resources/splash/splash-320x480.png',
+  'iphone_2x'         : 'resources/splash/splash-320x480@2x.png',  // 640x960
+  'iphone5'           : 'resources/splash/splash-320x568@2x.png',  // 640x1136
+  'iphone6'           : 'resources/splash/splash-375x667@2x.png',  // 750x1334
+  'iphone6p_portrait' : 'resources/splash/splash-414x736@3x.png',  // 1242x2208
+  'iphone6p_landscape': 'resources/splash/splash-736x414@3x.png',  // 2208x1242
+  'ipad_portrait'     : 'resources/splash/splash-768x1024.png',
+  'ipad_portrait_2x'  : 'resources/splash/splash-768x1024@2x.png', // 1536x2048
+  'ipad_landscape'    : 'resources/splash/splash-1024x768.png',
+  'ipad_landscape_2x' : 'resources/splash/splash-1024x768@2x.png', // 2048x1536
+});
