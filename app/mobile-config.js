6e0994750e51269d8219a552568eae9d7fccb3ba
Nobutaka OSHIRO <n-oshiro@j-hack.co.jp>
2016-01-17 11:39:27 +0900

    update app icons

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -3,7 +3,7 @@ App.info({
   id: 'com.example.meteor-todo-app', // アプリID
   name: 'Todo',                      // アプリ名
   description: 'Todo App',           // アプリ説明
-  author: 'J-hack Inc.',             // 作者
+  author: 'Example Inc.',            // 作者
   email: 'contact@example.com',      // メールアドレス
   website: 'http://example.com',     // ウェブサイト
   version: '0.1.0',                  // バージョン番号
@@ -15,6 +15,6 @@ App.icons({
   'iphone'   : 'resources/icons/app_icon-60x60.png',
   'iphone_2x': 'resources/icons/app_icon-60x60@2x.png', // 120x120
   'iphone_3x': 'resources/icons/app_icon-60x60@3x.png', // 180x180
-  'ipad'     : 'resources/icons/app_icon-72x72.png',
-  'ipad_2x'  : 'resources/icons/app_icon-72x72@2x.png', // 144x144
+  'ipad'     : 'resources/icons/app_icon-76x76.png',
+  'ipad_2x'  : 'resources/icons/app_icon-76x76@2x.png', // 144x144
 });
